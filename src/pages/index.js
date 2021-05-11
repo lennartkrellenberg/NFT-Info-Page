560f7b064123d2d0a5981288b99440fa39f3b01a
Lennart Krellenberg <lennart.krellenberg2002@gmail.com>
2021-05-11 23:41:31 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,295 @@
 import * as React from "react"
+import * as stylesheet from "../style/global.module.scss"
+import { graphql, useStaticQuery } from 'gatsby'
+import { Link } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 // markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+  const data = useStaticQuery(
+    graphql`
+    query MyQuery {
+      image: file(relativePath: {eq: "nfthero.jpg"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      
+      image1: file(relativePath: {eq: "hashmask.jpg"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      image2: file(relativePath: {eq: "cryptopunk.png"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      image3: file(relativePath: {eq: "bayc.png"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+
+      image4: file(relativePath: {eq: "beeple.jpg"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      
+      image5: file(relativePath: {eq: "ethboy.png"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      image6: file(relativePath: {eq: "bitcoin.png"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      image7: file(relativePath: {eq: "artboi.jpg"}) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+    
+      
+
+      
+      
+    `
   )
+  const imageData = data.image.childImageSharp.fluid;
+  const hashmask = data.image1.childImageSharp.fluid;
+  const cryptopunk = data.image2.childImageSharp.fluid;
+  const bayc = data.image3.childImageSharp.fluid;
+  const beeple = data.image4.childImageSharp.fluid;
+  const ethboy = data.image5.childImageSharp.fluid;
+  const bitcoin = data.image6.childImageSharp.fluid;
+  const artboi = data.image7.childImageSharp.fluid;
+
+
+  return (
+    <div>
+      <section id={stylesheet.hero}>
+        <BackgroundImage
+          className={stylesheet.background}
+          fluid={imageData}
+        >
+          <div className={stylesheet.heroContent}>
+            <h1>NFT - Non-Fungible Token </h1>
+          </div>
+        </BackgroundImage>
+      </section>
+      <div className={stylesheet.wrapper}>
+        <section id={stylesheet.what}>
+          <h2 className={stylesheet.h2Green}> Was sind NFTs?</h2>
+          <p className={stylesheet.pNormal}>Ein NFT ist ein Vermögenswert, der mithilfe der Blockchain-Technologie verifiziert wird, bei der ein Netzwerk von Computern Transaktionen aufzeichnet und Käufern einen Beweis für die Echtheit und das Eigentum liefert. <br /> Der aktuelle Boom gilt vor allem für digitale Assets, darunter Bilder, GIFs, Songs oder Videos. Vor allem aber machen NFTs digitale Kunstwerke einzigartig und damit verkaufbar. Jetzt nutzen Künstler, Musiker, Influencer und Sportfranchises NFTs, um digitale Güter zu monetarisieren, die bisher billig oder kostenlos waren. Die Technologie reagiert auch auf das Bedürfnis der Kunstwelt nach Authentifizierung und Provenienz in einer zunehmend digitalen Welt, indem sie eine digitale Datei dauerhaft mit ihrem Schöpfer verknüpft.</p>
+        </section>
+        <section id={stylesheet.art}>
+          <h2 className={stylesheet.h2Green}>Was hat das mit Kunst zutun?</h2>
+          <p className={stylesheet.pNormal}>
+            NFTs bieten Künstlern eine innovative Art, ihre Werke zu verkaufen – dieser digitale Vertriebsweg ist vor allem in Zeiten der Pandemie reizvoll. Durch ihren dezentralen Aufbau könnten Vermittlungsstellen wie  Auktionshäuser, wo man teilweise bis zu 25 Prozent Gebühren zahlen muss, umgangen werden. Mit Nfts haben auch unbekanntere Künstler die Chance, ihre Werke zu verkaufen und damit Gewinne zu erzielen. Zusätzlich lassen sich NFTs auch so programmieren, dass der Künstler bei jedem Weiterverkauf seines Werkes eine Provision erhält. <br />
+            Bekannt Marktplätze, wo man innerhalb weniger Minuten seine Kunstwerke anbieten kann, sind <Link className={stylesheet.link} to="https://rarible.com/">Rarible</Link>, <Link className={stylesheet.link} to="https://opensea.io/">OpenSea</Link> und <Link className={stylesheet.link} to="https://www.airnfts.com/">AirNFTs</Link>
+          </p>
+
+        </section>
+
+        <section id="Rekorde">
+
+          <div className={stylesheet.records}>
+            <div onClick={() => window.open("https://onlineonly.christies.com/s/beeple-first-5000-days/beeple-b-1981-1/112924?ldp_breadcrumb=back")} className={stylesheet.record}>
+              <h3>$69 Millionen</h3>
+              <p>teuerster NFT</p>
+            </div>
+
+            <div onClick={() => window.open("https://foundation.app/pak/-6544")} className={stylesheet.record}>
+              <h3>$1.559.492,28</h3>
+              <p>teuerstes GIF</p>
+            </div>
+
+
+            <div onClick={() => window.open("https://superrare.co/artwork-v2/mars-house-21383")} className={stylesheet.record}>
+              <h3>$512.000</h3>
+              <p>erstes 3D Kunstwerk</p>
+            </div>
+
+            <div onClick={() => window.open("https://nbatopshot.com/moment/bigdog_brothers+2499f572-8280-4057-ac27-5603971de95d")} className={stylesheet.record}>
+              <h3>$210.000</h3>
+              <p>teuerstes Collectable</p>
+            </div>
+
+
+          </div>
+        </section>
+
+        <section id="Beispiele">
+          <h2 className={stylesheet.h2Green}>Beispiele</h2>
+
+          <div className={stylesheet.examples}>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={hashmask}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://opensea.io/assets/0xc2c747e0f7004f9e8817db2ca4997657a7746928/6054")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+
+
+
+
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={cryptopunk}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://opensea.io/assets/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/7804")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={bayc}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => { window.open("https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8746") }}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={beeple}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => { window.open("https://opensea.io/assets/0x2a46f2ffd99e19a89476e2f62270e0a35bbf0756/50472") }}>Mehr Infos</button>
+              </div>
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={imageData}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://onlineonly.christies.com/s/beeple-first-5000-days/beeple-b-1981-1/112924?ldp_breadcrumb=back")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={ethboy}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://opensea.io/assets/0xb6dae651468e9593e4581705a09c10a76ac1e0c8/807")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={bitcoin}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://opensea.io/assets/0xb6dae651468e9593e4581705a09c10a76ac1e0c8/245")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+            <BackgroundImage
+              className={stylesheet.example}
+              fluid={artboi}
+            >
+              <div className={stylesheet.exampleOverlay}>
+                <div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                <button onClick={() => window.open("https://opensea.io/assets/0x60f80121c31a0d46b5279700f9df786054aa5ee5/230611")}>Mehr Infos</button>
+              </div>
+
+
+            </BackgroundImage>
+
+
+
+
+
+
+          </div>
+
+
+        </section>
+
+
+
+
+
+
+      </div>
+    </div >
+
+
+  )
+
 }
+
+
 
 export default IndexPage
